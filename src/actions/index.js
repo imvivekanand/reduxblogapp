511726b80b120784f0e3510bd4fe426eb0ec68e8
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2022-12-10 15:42:19 +0530

    Api added

---
+++ src/actions/index.js
@@ -0,0 +1,5 @@
+export const fetchPosts = () => {
+  return {
+    type: "Fetch_Posts",
+  };
+};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import App from './components/App';
+import reducers from './reducers';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode>Hello</React.StrictMode>);
+root.render(<Provider store={createStore(reducers)}><App/></Provider>);
